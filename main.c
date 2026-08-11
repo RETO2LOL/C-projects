3eadf09a840273efff33c8a33f16bb85c7667282
RETO2LOL <ammaryasseryasser49@gmail.com>
2026-08-11 14:10:27 +0300

    first commit

---
+++ main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+//lesson 1 basic input and output
+/*
+
+int main(){
+  printf("Hello, world!\n");
+  printf("nice\n");
+  return 0;
+}
+
+*/
+
+//lesson 2 variables
+
+/*
+int main(){
+
+  //this is a variable that can contain a value like a number but can't conatin a dicemal number
+  //int age = 30;
+  //printf("you are %d years old\n",age);
+
+  // and this is float that can conatin also a value like a number but this one can conatin dicemal number we have also double the deffrece is that float can have 4 byts but double can have 8
+  //float price = 9.99;
+  //printf("product price is %f \n",price);
+  //double pi = 3.14159265358979;
+  //printf("pi is %.15lf\n", pi);
+
+  //we also have char it can contain a charecter a single charecter but sadly in c there is nothing called string we can only store one char if you want to store a more than one charecter you can use an array
+  //an array smth that allow you to store more than one value to use it 
+
+  //char grade = 'a';
+  //printf("your grade is %c\n",grade);
+
+  //char name[] = "ammar";
+  //printf("my name is %s\n",name);
+  //return 0;
+  
+  //and finnaly we have boolean that can store value of true or false only to use it we have to incude stdbool
+  //bool isOnline = true;
+  /*
+    if(isOnline){
+      printf("You are online");
+    }
+    else{
+     printf("You are offline");
+    }
+  */
+}
+
