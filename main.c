03bfc1e9902d0190a6e9fabb466dbc8b05846210
RETO2LOL <ammaryasseryasser49@gmail.com>
2026-08-12 12:29:05 +0300

    lesson 3 format specifier

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
 
-//lesson 1 basic input and output
+// lesson 1 basic input and output
 /*
 
 int main(){
@@ -12,23 +12,28 @@ int main(){
 
 */
 
-//lesson 2 variables
+// lesson 2 variables
 
 /*
 int main(){
 
-  //this is a variable that can contain a value like a number but can't conatin a dicemal number
+  //this is a variable that can contain a value like a number but can't conatin
+a dicemal number
   //int age = 30;
   //printf("you are %d years old\n",age);
 
-  // and this is float that can conatin also a value like a number but this one can conatin dicemal number we have also double the deffrece is that float can have 4 byts but double can have 8
+  // and this is float that can conatin also a value like a number but this one
+can conatin dicemal number we have also double the deffrece is that float can
+have 4 byts but double can have 8
   //float price = 9.99;
   //printf("product price is %f \n",price);
   //double pi = 3.14159265358979;
   //printf("pi is %.15lf\n", pi);
 
-  //we also have char it can contain a charecter a single charecter but sadly in c there is nothing called string we can only store one char if you want to store a more than one charecter you can use an array
-  //an array smth that allow you to store more than one value to use it 
+  //we also have char it can contain a charecter a single charecter but sadly in
+c there is nothing called string we can only store one char if you want to store
+a more than one charecter you can use an array
+  //an array smth that allow you to store more than one value to use it
 
   //char grade = 'a';
   //printf("your grade is %c\n",grade);
@@ -36,8 +41,9 @@ int main(){
   //char name[] = "ammar";
   //printf("my name is %s\n",name);
   //return 0;
-  
-  //and finnaly we have boolean that can store value of true or false only to use it we have to incude stdbool
+
+  //and finnaly we have boolean that can store value of true or false only to
+use it we have to incude stdbool
   //bool isOnline = true;
   /*
     if(isOnline){
@@ -47,5 +53,43 @@ int main(){
      printf("You are offline");
     }
   */
-}
+//}
 
+// lesson 3 formant specifiers it is a Special tokens that begin with a %
+// symbol, folowed by a character that specifiers the data
+// int main() {
+//
+//   // int num = 15;
+//   // float price = 19.99;
+//   // double pi = 3.141592;
+//   // char currency = '$';
+//   // char name[] = "ammar";
+//   // printf("%d\n", num);
+//   // printf("%f\n", price);
+//   // printf("%f\n", pi);
+//   // printf("%c\n", currency);
+//   // printf("%s\n", name);
+//
+//   // width
+//   // int num1 = 1;
+//   // int num2 = 10;
+//   // int num3 = -100;
+//   //
+//   // printf("%3d\n", num1);
+//   // printf("%-4d\n", num2);
+//   // printf("%+4d\n", num3);
+//
+//   // precision
+//   float price1 = 19.99;
+//   float price2 = 1.99;
+//   float price3 = 14.99;
+//
+//   printf("%.2f\n", price1);
+//   printf("%.3f\n", price2);
+//   printf("%.5f\n", price3);
+//
+//   return 0;
+// }
+//
+
+int main() { return 0; }
