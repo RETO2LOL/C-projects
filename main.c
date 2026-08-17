43c84b494dc1aa830fb3739108abb38a11842bc5
RETO2LOL <ammaryasseryasser49@gmail.com>
2026-08-17 12:57:41 +0300

    2nd project

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
+#include <string.h>
 
 // lesson 1 basic input and output
 /*
@@ -134,26 +135,65 @@ use it we have to incude stdbool
 //   return 0;
 // }
 
+// cart project
+//  int main() {
+//    // declarig variables
+//    char TypeOfFood[30] = "";
+//    float PriceofFood = 0.0f;
+//    int HowMuch = 0;
+//
+//    // getting the input from the user
+//    printf("What do you need: ");
+//    fgets(TypeOfFood, sizeof(TypeOfFood), stdin);
+//
+//    printf("How much do you want for it:");
+//    scanf("%f", &PriceofFood);
+//
+//    printf("How much do you want:");
+//    scanf("%d", &HowMuch);
+//
+//    // adding the output for the user
+//    float Total = PriceofFood * HowMuch;
+//    printf("you have bought %d %s", HowMuch, TypeOfFood);
+//    printf("your Total is %.1f\n", Total);
+//
+//    return 0;
+//  }
+
+// game project
 int main() {
   // declarig variables
-  char TypeOfFood[30] = "";
-  float PriceofFood = 0.0f;
-  int HowMuch = 0;
+  char description[50] = "";
+  char noun[50] = "";
+  char adjective[50] = "";
+  char verb[50] = "";
+  char adjective2[50] = "";
 
-  // getting the input from the user
-  printf("What do you need: ");
-  fgets(TypeOfFood, sizeof(TypeOfFood), stdin);
+  // getting input from the user
+  printf("Enter an adjective (description): ");
+  fgets(description, sizeof(description), stdin);
+  description[strlen(description) - 1] = '\0';
 
-  printf("How much do you want for it:");
-  scanf("%f", &PriceofFood);
+  printf("Enter a noun(animal or person): ");
+  fgets(noun, sizeof(noun), stdin);
+  noun[strlen(noun) - 1] = '\0';
 
-  printf("How much do you want:");
-  scanf("%d", &HowMuch);
+  printf("Enter a adjective(description): ");
+  fgets(adjective, sizeof(adjective), stdin);
+  adjective[strlen(adjective) - 1] = '\0';
 
-  // adding the output for the user
-  float Total = PriceofFood * HowMuch;
-  printf("you have bought %d %s", HowMuch, TypeOfFood);
-  printf("your Total is %.1f\n", Total);
+  printf("Enter a verb(ending /w -ing): ");
+  fgets(verb, sizeof(verb), stdin);
+  verb[strlen(verb) - 1] = '\0';
+
+  printf("Enter a adjective(description): ");
+  fgets(adjective2, sizeof(adjective2), stdin);
+  adjective2[strlen(adjective2) - 1] = '\0';
+
+  printf("Today I went to a %s.\n", description);
+  printf("In an exhibit, I saw a  %s.\n", noun);
+  printf("%s was %s and %s.\n", noun, adjective, verb);
+  printf("I was %s\n", adjective2);
 
   return 0;
 }
